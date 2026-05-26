0c9c829bd8aaed9569bdd7bec76fd25b6039a9fe
João Figueredo <63759223+jpfigueredo@users.noreply.github.com>
2026-05-26 18:02:31 -0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        // remove special characters and spaces
+        // check if the size is even
+        // We can do 2 things here, substrings cutting the string in half and reversing the second one to check if they are equal
+        // or do a two pointer from right to left and left to right and finish in the middle
+        int left = 0;
+        int right = s.length()-1;
+        char[] word = s.toCharArray();
+
+        // O(n/2)
+        while(left < right){
+            if(word[left] != word[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+}
