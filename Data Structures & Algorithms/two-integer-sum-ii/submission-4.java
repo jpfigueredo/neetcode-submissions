9605392397df44a07a3ea98a5996bcd4fbc25d61
João Figueredo <63759223+jpfigueredo@users.noreply.github.com>
2026-05-26 18:50:00 -0300

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length-1;
+
+        while(left < right){
+            if(numbers[left]+numbers[right]==target){
+                return new int[]{left+1, right+1};
+            }
+            
+            int complement = target-numbers[left];
+            if(numbers[right] > complement){
+                right--;
+            } else {
+                left++;
+            }
+            
+        }
+        return null;
+    }
+}
